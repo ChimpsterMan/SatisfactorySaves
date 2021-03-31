3f57d7ea52ae0ff3e5c682c6968c967d88a46fe4
ChimpsterMan <emnilsson007@gmail.com>
2021-03-31 14:04:33 -0400

    User Called Sync

--- Remote Management/setup.bat
+++ Remote Management/setup.bat
@@ -1,6 +1,6 @@
 git init
 git remote add origin https://github.com/ChimpsterMan/SatisfactorySaves.git
-git pull origin master
+git pull
 git add .
 git commit -m "New user joined save repo"
 git push -u origin master
